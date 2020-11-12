ef1c936170ee8c977c28c6bb67f3018beba2ec6d
Andrey <kouznts@ya.ru>
2020-11-12 20:18:19 +0400

    impl random coords arr method

--- src/nbodygui/Frame.java
+++ src/nbodygui/Frame.java
@@ -54,4 +54,20 @@ public class Frame extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+    private Coords[] randomCoordsArr(int num, Dimension bounds) {
+
+        Coords[] randomCoordsArr = new Coords[num];
+
+        int x, y;
+        Random random = new Random();
+
+        for (int i = 0; i < randomCoordsArr.length; i++) {
+            x = Math.abs(random.nextInt()) % bounds.width;
+            y = Math.abs(random.nextInt()) % bounds.height;
+            randomCoordsArr[i] = new Coords(x, y);
+        }
+
+        return randomCoordsArr;
+    }
 }
