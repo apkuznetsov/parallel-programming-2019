739767c43c965e001c32acdcf35f946bb405114f
Andrey <kouznts@ya.ru>
2020-11-12 19:55:47 +0400

    refactor title constant in frame

--- src/nbodygui/Frame.java
+++ src/nbodygui/Frame.java
@@ -11,13 +11,11 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Random;
 
-import static nbodygui.Panels.TITLE;
-
 public class Frame extends JFrame {
 
     public Frame() {
-        super(TITLE);
-        initGui();
+        super(Panels.TITLE);
+        init();
     }
 
     public static void main(String[] args) {
