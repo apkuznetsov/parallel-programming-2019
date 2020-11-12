647eadd7b554400af0102c7412ee5c0762ee73e7
Andrey <kouznts@ya.ru>
2020-11-12 20:51:41 +0400

    copy random coords arr method

--- src/App.java
+++ src/App.java
@@ -1,10 +1,30 @@
+import nbody.Coords;
 import nbody.NbodySolvers;
 import nbodygui.Frame;
+
+import java.awt.*;
+import java.util.Random;
 
 public class App {
 
     public static void main(String[] args) {
         Frame frame = new Frame(NbodySolvers.MIN_BODIES_NUM);
         frame.setVisible(true);
+    }
+
+    public static Coords[] randomCoordsArr(int num, Dimension bounds) {
+
+        Coords[] randomCoordsArr = new Coords[num];
+
+        int x, y;
+        Random random = new Random();
+
+        for (int i = 0; i < randomCoordsArr.length; i++) {
+            x = Math.abs(random.nextInt()) % bounds.width;
+            y = Math.abs(random.nextInt()) % bounds.height;
+            randomCoordsArr[i] = new Coords(x, y);
+        }
+
+        return randomCoordsArr;
     }
 }
