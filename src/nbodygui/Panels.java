ae5582675b5137334ae92a36a15c4d66608b9494
Andrey <kouznts@ya.ru>
2020-11-13 10:01:45 +0400

    refactor points constants

--- src/nbodygui/Panels.java
+++ src/nbodygui/Panels.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 public class Panels {
 
-    public static final Color DEFAULT_POINT_COLOR = Color.BLACK;
-    public static final int DEFAULT_POINT_SIZE = 10;
+    public static final Color DEFAULT_POINTS_COLOR = Color.BLACK;
+    public static final int DEFAULT_POINTS_SIZE = 10;
     public static final int MIN_DURATION_MILLIS = 10_000;
     public static final int MAX_DURATION_MILLIS = 60_000;
 }
