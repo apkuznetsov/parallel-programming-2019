92a811e0c6abb1deb032c5f560588efb55c960e0
Andrey Kuznetsov <ethenaut@ya.ru>
2023-12-28 06:19:04 +0400

    def params parser defaults

---
+++ n-body-problem/src/kuznetsov/nbodyproblemgui/ParamsParserDefaults.java
@@ -0,0 +1,23 @@
+package kuznetsov.nbodyproblemgui;
+
+public class ParamsParserDefaults {
+
+    public static final String KEY_BODIES_NUM = "bodiesNum";
+    public static final String KEY_BODY_MASS = "bodyMass";
+    public static final String KEY_DELTA_TIME = "deltaTime";
+    public static final String KEY_ERROR_DISTANCE = "errorDistance";
+    public static final String KEY_WIDTH = "width";
+    public static final String KEY_HEIGHT = "height";
+    public static final String KEY_DURATION_SECONDS = "durationSeconds";
+    public static final String KEY_THREADS_NUM = "threadsNum";
+
+    public static final int DEFAULT_BODIES_NUM = 4;
+    public static final double DEFAULT_BODY_MASS = 1e10;
+    public static final int DEFAULT_DELTA_TIME = 16;
+    public static final double DEFAULT_ERROR_DISTANCE = 1e2;
+    public static final int DEFAULT_WIDTH = 500;
+    public static final int DEFAULT_HEIGHT = 500;
+    public static final int DEFAULT_DURATION_MILLIS = 10_000;
+    public static final int DEFAULT_THREADS_NUM = 1;
+
+}
